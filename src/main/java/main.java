81079e74b79cd7bd54a09f1220502076973d60ea
Akkarim <luisda7cr@gmail.com>
2017-11-05 02:47:35 -0600

    PRB

---
+++ src/main/java/main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class main {
+
+
+    public static void main(String[] args) /*throws IOException*/ {
+
+        System.out.println("Hola putitos");
+
+    }
+
+
+
+
+
+
+}
